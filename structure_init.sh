2c0b1a7e8e1a835a1ca003f7efe7cdf382aecc81
Daniil Roman <d.roman@just-ai.com>
2020-10-07 22:51:27 +0300

    [task 1] enrich structure_init.sh

--- structure_init.sh
+++ structure_init.sh
@@ -1,3 +1,6 @@
 mkdir ./data
-mkdir ./data/archive 
+mkdir ./data/archive
 mkdir ./data/features
+
+curl https://pjreddie.com/media/files/mnist_train.csv --output ./data/archive/mnist_train.csv
+curl https://pjreddie.com/media/files/mnist_test.csv --output ./data/archive/mnist_test.csv
